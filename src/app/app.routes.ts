0b3e0d550f264f0e8e48038589059b8383d4743d
Anabelly Passos <123272360+anabellypassos@users.noreply.github.com>
2025-09-04 12:13:40 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EmpresaComponent } from './empresa/empresa.component';
+import e from 'express';
+import { ServicosComponent } from './servicos/servicos.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+{path:'Home', component: HomeComponent},
+{path:'Empresa', component:EmpresaComponent},
+{path:'Servicos', component:ServicosComponent},
+{path:'Clientes', component: ClientesComponent},
+{path:'Blog', component:BlogComponent},
+{path:'Contato', component:ContatoComponent},
+ { path: '', redirectTo: '/Home', pathMatch: 'full' },
+{ path: '**', redirectTo: '/Home' }
+
+
+
+
+];
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterLinkActive],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
