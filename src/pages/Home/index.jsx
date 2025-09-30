2954ae260079331c4a9dfc20805b2bc9f786bda8
Anabelly Passos <123272360+anabellypassos@users.noreply.github.com>
2025-09-30 16:41:37 -0300

    title

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,12 @@ function Home(){
     return(
         <div>
   <Header/>
+    <div className="quadrante01">
+      <h1 className="Titledestac">
+        Consultoria especializada em <span>desenvolvimento de software customizados</span>
+      </h1>
+      <p className="paragrapy01">Apoiamos esmpresas não nativas digitais a <span>digitalizar processos críticos</span> e criar novas linhas de receita com produtos ou sistemas criados <span>sob medida</span></p>
+    </div>
         </div>
     )
 }
