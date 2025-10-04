3fac1f3b562510173f0798c04a9ba54ff6ca0b43
Anabelly Passos <123272360+anabellypassos@users.noreply.github.com>
2025-10-04 18:10:59 -0300

    slider image logo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,16 +8,28 @@ import imageslider02 from '../../assets/Midia/imagens/conceito-de-fundo-de-progr
 import imageslider03 from '../../assets/Midia/imagens/engenheiro-de-computadores-digitar-no-teclado-escrever-codigo-para-construir-firewalls.jpg'
 import imageslider04 from '../../assets/Midia/imagens/tela-de-digitalizacao-de-programador-em-seu-smartwatch-com-camera-smartphone.jpg'
 import imageslider05 from '../../assets/Midia/imagens/trabalhando-no-codigo.jpg'
+/*Logos do carrossel*/ 
+import logo1 from '../../assets/Midia/imagens/logosdeempresas/logo1.png';
+import logo2 from '../../assets/Midia/imagens/logosdeempresas/logo2.jpg';
+import logo3 from '../../assets/Midia/imagens/logosdeempresas/logo3.png';
+import logo4 from '../../assets/Midia/imagens/logosdeempresas/logo4.png';
+import logo5 from '../../assets/Midia/imagens//logosdeempresas/logo5.png';
+import logo6 from '../../assets/Midia/imagens/logosdeempresas/logo6.webp';
+import logo7 from '../../assets/Midia/imagens/logosdeempresas/logo7.png';
+import logo8 from '../../assets/Midia/imagens/logosdeempresas/logo8.png';
+import logo9 from '../../assets/Midia/imagens/logosdeempresas/logo9.png';
+import logo10 from '../../assets/Midia/imagens/logosdeempresas/logo10.png';
+import logo11 from '../../assets/Midia/imagens/logosdeempresas/logo11.png';
+import logo12 from '../../assets/Midia/imagens/logosdeempresas/logo12.PNG';
+/* Fim das logos do carrosel*/ 
 
 function Home(){
   //useState(0) cria uma váriavel de estado chamada indice atual.
   //Ela começa com 0, que significa que a primeira imagem da lista será mostrada
   //setIndiceAtual é a função que vai usar para mudar o indice
   //listaDeImagens é um array com todas as imagens que vai mostrar no slider.
-   const [indiceAtual,setIndiceAtual]= useState(0)
-   const listaDeImagens = [imageslider0,imageslider01,imageslider02,imageslider03,imageslider04,imageslider05];
-
-
+  const [indiceAtual,setIndiceAtual]= useState(0)
+  const listaDeImagens = [imageslider0,imageslider01,imageslider02,imageslider03,imageslider04,imageslider05];
    //useEffect é usado para executar algo depois que o componente é renderizado
    //Nesse caso ele inicia o intervalo de troca de imagens.
    //setInterval(() => { ... }, 3000) executa a função a cada 3000 milessegundos 
@@ -30,7 +42,7 @@ function Home(){
       return () => clearInterval(intervalo);
     }, [listaDeImagens.length]); 
     return(
-        <div>
+        <div className="backgroundhome">
   <Header/>
     <div className="quadrante01">
   <div className="conteudo-texto">
@@ -53,9 +65,28 @@ function Home(){
       alt="imagem"
     />
   </div>
-</div>
-   
+
+
+  </div> 
+   <h1 className="Nossos-clientes">Nossos Clientes</h1>
+   <div className="carrossel">
+    <div className="container" id="img">
+    <img src={logo1} alt="logo1"></img>
+     <img src={logo2} alt="logo2"></img>
+      <img src={logo3} alt="logo3"></img>
+       <img src={logo4} alt="logo4"></img>
+        <img src={logo5} alt="logo5"></img>
+         <img src={logo6} alt="logo6"></img>
+          <img src={logo7} alt="logo7"></img>
+           <img src={logo8} alt="logo8"></img>
+            <img src={logo9} alt="logo9"></img>
+             <img src={logo10} alt="logo10"></img>
+              <img src={logo11} alt="logo11"></img>
+               <img src={logo12} alt="logo12"></img>
+   </div>
+   </div>
         </div>
+       
     )
 }
 
