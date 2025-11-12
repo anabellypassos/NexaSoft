8ab7c27c9aa5a8a1961baf2be174e1d3c0c2cbb7
Anabelly Passos <123272360+anabellypassos@users.noreply.github.com>
2025-11-12 16:30:53 -0300

    section02

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,7 +11,7 @@ import imageslider02 from '../../assets/Midia/imagens/conceito-de-fundo-de-progr
 import imageslider03 from '../../assets/Midia/imagens/engenheiro-de-computadores-digitar-no-teclado-escrever-codigo-para-construir-firewalls.jpg'
 import imageslider04 from '../../assets/Midia/imagens/tela-de-digitalizacao-de-programador-em-seu-smartwatch-com-camera-smartphone.jpg'
 import imageslider05 from '../../assets/Midia/imagens/trabalhando-no-codigo.jpg'
-/*Logos do carrossel*/ 
+/*Logos do carrossel*/
 import logo1 from '../../assets/Midia/imagens/logosdeempresas/logo1.png';
 import logo2 from '../../assets/Midia/imagens/logosdeempresas/logo2.jpg';
 import logo3 from '../../assets/Midia/imagens/logosdeempresas/logo3.png';
@@ -24,7 +24,7 @@ import logo9 from '../../assets/Midia/imagens/logosdeempresas/logo9.png';
 import logo10 from '../../assets/Midia/imagens/logosdeempresas/logo10.png';
 import logo11 from '../../assets/Midia/imagens/logosdeempresas/logo11.png';
 import logo12 from '../../assets/Midia/imagens/logosdeempresas/logo12.PNG';
-/* Fim das logos do carrosel*/ 
+/* Fim das logos do carrosel*/
 
 /** Carrosel serviços*/
 import servico1 from "../../assets/Midia/imagens/imagemdeconsultorias/servico1.jpg";
@@ -34,24 +34,24 @@ import servico4 from "../../assets/Midia/imagens/imagemdeconsultorias/servico4.w
 import servico5 from "../../assets/Midia/imagens/imagemdeconsultorias/servico5.jpg";
 /* Fim das imagens de serviços*/
 
-function Home(){
+function Home() {
   //useState(0) cria uma váriavel de estado chamada indice atual.
   //Ela começa com 0, que significa que a primeira imagem da lista será mostrada
   //setIndiceAtual é a função que vai usar para mudar o indice
   //listaDeImagens é um array com todas as imagens que vai mostrar no slider.
-  const [indiceAtual,setIndiceAtual]= useState(0)
-  const listaDeImagens = [imageslider0,imageslider01,imageslider02,imageslider03,imageslider04,imageslider05];
-   //useEffect é usado para executar algo depois que o componente é renderizado
-   //Nesse caso ele inicia o intervalo de troca de imagens.
-   //setInterval(() => { ... }, 3000) executa a função a cada 3000 milessegundos 
+  const [indiceAtual, setIndiceAtual] = useState(0)
+  const listaDeImagens = [imageslider0, imageslider01, imageslider02, imageslider03, imageslider04, imageslider05];
+  //useEffect é usado para executar algo depois que o componente é renderizado
+  //Nesse caso ele inicia o intervalo de troca de imagens.
+  //setInterval(() => { ... }, 3000) executa a função a cada 3000 milessegundos 
   useEffect(() => {
-      const intervalo = setInterval(() => {
-        setIndiceAtual(indiceAntigo => (indiceAntigo + 1) % listaDeImagens.length);
-      }, 3000); // Troca a imagem a cada 3 segundos (3000 milissegundos)
+    const intervalo = setInterval(() => {
+      setIndiceAtual(indiceAntigo => (indiceAntigo + 1) % listaDeImagens.length);
+    }, 3000); // Troca a imagem a cada 3 segundos (3000 milissegundos)
 
-      // Limpar o intervalo quando o componente for desmontado
-      return () => clearInterval(intervalo);
-    }, [listaDeImagens.length]); 
+    // Limpar o intervalo quando o componente for desmontado
+    return () => clearInterval(intervalo);
+  }, [listaDeImagens.length]);
 
 
   var settings = {
@@ -61,7 +61,7 @@ function Home(){
     slidesToShow: 8,
     slidesToScroll: 8,
     initialSlide: 0,
-      autoplay: true,
+    autoplay: true,
     autoplaySpeed: 8000,
     cssEase: "linear",
     responsive: [
@@ -91,10 +91,10 @@ function Home(){
       }
     ]
   };
-   
 
 
 
+ 
   var sliderservicos = {
     dots: true,
     infinite: true,
@@ -108,16 +108,16 @@ function Home(){
     responsive: [
       {
         breakpoint: 1024,
-       sliderservicos: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+        settings: { 
+          slidesToShow: 2,
+          slidesToScroll: 2,
           infinite: true,
           dots: true
         }
       },
       {
         breakpoint: 600,
-       sliderservicos: {
+        settings: { 
           slidesToShow: 2,
           slidesToScroll: 2,
           initialSlide: 2
@@ -125,81 +125,121 @@ function Home(){
       },
       {
         breakpoint: 480,
-       sliderservicos: {
+        settings: { 
           slidesToShow: 1,
           slidesToScroll: 1
         }
       }
     ]
   };
-   
-  
-    return(
-        <div className="backgroundhome">
-  <Header/>
-    <div className="quadrante01">
-  <div className="conteudo-texto">
-    <h1 className="Titledestac">
-      Consultoria especializada em <span>desenvolvimento de software customizados</span>
-    </h1>
-    <p className="paragrapy01">
-      Apoiamos empresas não nativas digitais a <span>digitalizar processos críticos</span> 
-      e criar novas linhas de receita com produtos ou sistemas criados <span>sob medida</span>
-    </p>
-    <div className="button">
-      <button className="buttonprimary">Fale com especialista</button>
-      <button className="buttonprimary">Agende uma reunião</button>
-    </div>
-  </div>
-
-  <div className="slider">
-    <img
-      src={listaDeImagens[indiceAtual]}
-      alt="imagem"
-    />
-  </div>
-
-
-  </div> 
-   <h1 className="Nossos-clientes">Nossos Clientes</h1>
-   <div className="carrossel">
-      <Slider {...settings}>
-    <img src={logo1} alt="logo1"></img>
-     <img src={logo2} alt="logo2"></img>
-      <img src={logo3} alt="logo3"></img>
-       <img src={logo4} alt="logo4"></img>
-        <img src={logo5} alt="logo5"></img>
-         <img src={logo6} alt="logo6"></img>
-          <img src={logo7} alt="logo7"></img>
-           <img src={logo8} alt="logo8"></img>
-            <img src={logo9} alt="logo9"></img>
-             <img src={logo10} alt="logo10"></img>
-              <img src={logo11} alt="logo11"></img>
-               <img src={logo12} alt="logo12"></img>
-    </Slider>
-   </div>
-  
-  <div>
-    <div className="quadrante02">
-    <div className="Text02">
-  <h1>Nossos <span>Serviços</span> </h1>
-  <p>Empresas líderes na era digital precisam de um parceiro especializado como a CSP Tech para superar a concorrência. Entendemos seus desafios e mostramos as melhores opções para resolvê-los.</p>
-    </div>
-    <div className="carrosselServicos">
-      <Slider{...sliderservicos}>
-        <img src={servico1} alt="servico1" />
-        <img src={servico2} alt="servico2" />
-        <img src={servico3} alt="servico3" />
-        <img src={servico4} alt="servico4" />
-        <img src={servico5} alt="servico5" />
-      </Slider>
-    </div>
-    </div>
-  </div>
-
+  return (
+    <div className="backgroundhome">
+      <Header />
+      <div className="quadrante01">
+        <div className="conteudo-texto">
+          <h1 className="Titledestac">
+            Consultoria especializada em <span>desenvolvimento de software customizados</span>
+          </h1>
+          <p className="paragrapy01">
+            Apoiamos empresas não nativas digitais a <span>digitalizar processos críticos</span>
+            e criar novas linhas de receita com produtos ou sistemas criados <span>sob medida</span>
+          </p>
+          <div className="button">
+            <button className="buttonprimary">Fale com especialista</button>
+            <button className="buttonprimary">Agende uma reunião</button>
+          </div>
         </div>
-       
-    )
+
+        <div className="slider">
+          <img
+            src={listaDeImagens[indiceAtual]}
+            alt="imagem"
+          />
+        </div>
+
+
+      </div>
+      <h1 className="Nossos-clientes">Nossos Clientes</h1>
+      <div className="carrossel">
+        <Slider {...settings}>
+          <img src={logo1} alt="logo1"></img>
+          <img src={logo2} alt="logo2"></img>
+          <img src={logo3} alt="logo3"></img>
+          <img src={logo4} alt="logo4"></img>
+          <img src={logo5} alt="logo5"></img>
+          <img src={logo6} alt="logo6"></img>
+          <img src={logo7} alt="logo7"></img>
+          <img src={logo8} alt="logo8"></img>
+          <img src={logo9} alt="logo9"></img>
+          <img src={logo10} alt="logo10"></img>
+          <img src={logo11} alt="logo11"></img>
+          <img src={logo12} alt="logo12"></img>
+        </Slider>
+      </div>
+
+      <div>
+        <div className="quadrante02">
+          <div className="Text02">
+            <h1>Nossos <span>Serviços</span> </h1>
+            <p>Empresas líderes na era digital precisam de um parceiro especializado como a CSP Tech para superar a concorrência. Entendemos seus desafios e mostramos as melhores opções para resolvê-los.</p>
+          </div>
+          <div className="carrosselServicos">
+            <Slider{...sliderservicos}>
+              <div className="servico-card">
+                <img src={servico1} alt="servico1" className="imagem-servico" />
+                <div className="servico-overlay">
+                  <h3 className="servico-titulo">Design UI UX</h3>
+                  <a href="/servico1" className="botao-saiba-mais">Saiba Mais</a>
+                </div>
+              </div>
+
+              <div className="servico-card">
+                <img src={servico2} alt="servico2" className="imagem-servico" />
+                <div className="servico-overlay">
+                  <h3 className="servico-titulo">Solution Development</h3>
+
+                  <a href="/servico2" className="botao-saiba-mais">Saiba Mais</a>
+                </div>
+              </div>
+
+              <div className="servico-card">
+
+
+                <img src={servico3} alt="servico3" className="imagem-servico" />
+                <div className="servico-overlay">
+                  <h3 className="servico-titulo">Smart Process</h3>
+                  <a href="/servico3" className="botao-saiba-mais">Saiba Mais</a>
+                </div>
+              </div>
+
+              <div className="servico-card">
+                <img src={servico4} alt="servico4" className="imagem-servico" />
+                <div className="servico-overlay">
+                  <h3 className="servico-titulo">Consultoria em Jira</h3>
+
+                  <a href="/servico4" className="botao-saiba-mais">Saiba Mais</a>
+                </div>
+
+              </div>
+
+              <div className="servico-card">
+                <img src={servico5} alt="servico5" className="imagem-servico" />
+                <div className="servico-overlay">
+                  <h3 className="servico-titulo">Data & AI
+ </h3>
+
+                  <a href="/servico5" className="botao-saiba-mais">Saiba Mais</a>
+                </div>
+
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  )
 }
 
 export default Home;
