72ac8edf12aacf9bc069cb6dab057aeb42bdcabc
Anabelly Passos <123272360+anabellypassos@users.noreply.github.com>
2025-11-18 13:50:59 -0300

    seccao 3

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -35,21 +35,15 @@ import servico5 from "../../assets/Midia/imagens/imagemdeconsultorias/servico5.j
 /* Fim das imagens de serviços*/
 
 function Home() {
-  //useState(0) cria uma váriavel de estado chamada indice atual.
-  //Ela começa com 0, que significa que a primeira imagem da lista será mostrada
-  //setIndiceAtual é a função que vai usar para mudar o indice
-  //listaDeImagens é um array com todas as imagens que vai mostrar no slider.
+
   const [indiceAtual, setIndiceAtual] = useState(0)
   const listaDeImagens = [imageslider0, imageslider01, imageslider02, imageslider03, imageslider04, imageslider05];
-  //useEffect é usado para executar algo depois que o componente é renderizado
-  //Nesse caso ele inicia o intervalo de troca de imagens.
-  //setInterval(() => { ... }, 3000) executa a função a cada 3000 milessegundos 
+
   useEffect(() => {
     const intervalo = setInterval(() => {
       setIndiceAtual(indiceAntigo => (indiceAntigo + 1) % listaDeImagens.length);
-    }, 3000); // Troca a imagem a cada 3 segundos (3000 milissegundos)
+    }, 3000);
 
-    // Limpar o intervalo quando o componente for desmontado
     return () => clearInterval(intervalo);
   }, [listaDeImagens.length]);
 
@@ -94,7 +88,7 @@ function Home() {
 
 
 
- 
+
   var sliderservicos = {
     dots: true,
     infinite: true,
@@ -108,7 +102,7 @@ function Home() {
     responsive: [
       {
         breakpoint: 1024,
-        settings: { 
+        settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
           infinite: true,
@@ -117,7 +111,7 @@ function Home() {
       },
       {
         breakpoint: 600,
-        settings: { 
+        settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
           initialSlide: 2
@@ -125,7 +119,7 @@ function Home() {
       },
       {
         breakpoint: 480,
-        settings: { 
+        settings: {
           slidesToShow: 1,
           slidesToScroll: 1
         }
@@ -226,7 +220,7 @@ function Home() {
                 <img src={servico5} alt="servico5" className="imagem-servico" />
                 <div className="servico-overlay">
                   <h3 className="servico-titulo">Data & AI
- </h3>
+                  </h3>
 
                   <a href="/servico5" className="botao-saiba-mais">Saiba Mais</a>
                 </div>
@@ -234,9 +228,23 @@ function Home() {
               </div>
             </Slider>
           </div>
+    
         </div>
       </div>
-
+      <div className="quadrante3">
+            <div className="bluor"></div>
+            <div className="texts">
+              <h1> Décadas de história ajudando a<span> transformar negócios e cocriar experiências digitais</span></h1>
+              <p>"Com o modelo implantado pela CSP, conseguimos maior volume de entregas, ganhamos produtividade,
+                rapidez no tempo de resposta e redução de custos.
+                Na parte de Business Intelligence, os painéis
+                desenvolvidos pela CSP chegam até o nível executivo,
+                com resultados e dados que apoiam nossa melhor tomada de decisão."
+              </p>
+              
+            </div>
+            <p className="grupy">Betsy Rabello, Ex-CIO do Grupo Ipiranga</p>
+          </div>
     </div>
 
   )
