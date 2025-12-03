f6ce6342f91f9362076b028dcb0cbcbbb27f826e
Anabelly Passos <anabellybel123@gmail.com>
2025-12-03 16:05:44 -0300

    Configurando ao ReactRouterDom

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from './pages/Home'
+import AppRoutes from './routes'; // O build system achará o .jsx automaticamente
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home/>
+    <AppRoutes />
   </StrictMode>,
 )
